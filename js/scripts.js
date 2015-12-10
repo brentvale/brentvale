0a0daf10d6220b57af2c2ccd37534cfca9fede39
Brent Vale <brentvale@gmail.com>
2015-12-10 11:53:53 -0800

    lazy loads youtube videos to improve initial load

--- js/scripts.js
+++ js/scripts.js
@@ -5,19 +5,18 @@ $(function(){
     TITLE_INDEX: 0,
     BLINK_INTERVAL: "",
     JOB_TITLE_CURRENT: "", //building the job title string, keeps track of position
-    JOB_TITLE_SPAN: "<span class='active'></span>"
+    JOB_TITLE_SPAN: "<span class='blinking'></span>"
   };
   
   var addNextCharacter = function(){
     var $jobTitle = $("#jobTitle");
-    var jobTitleText = $jobTitle.html();
-   
     if(CONSTANTS.TITLE_INDEX < CONSTANTS.JOB_TITLE.length){
       CONSTANTS.JOB_TITLE_CURRENT += CONSTANTS.JOB_TITLE[CONSTANTS.TITLE_INDEX];
       $jobTitle.html(CONSTANTS.JOB_TITLE_CURRENT + CONSTANTS.JOB_TITLE_SPAN);
       CONSTANTS.TITLE_INDEX += 1;
     } else {
       clearInterval(CONSTANTS.LETTERING_INTERVAL);
+      loadYouTubeVideos();
     }
   };
   
@@ -25,6 +24,13 @@ $(function(){
     CONSTANTS.LETTERING_INTERVAL = setInterval(function(){
       addNextCharacter();
     }, 60);
+  };
+  
+  var loadYouTubeVideos = function(){
+    $("#gardeningYouTube").attr("src", "https://www.youtube.com/embed/mpHgUCmmfpc");
+    $("#boomerangYouTube").attr("src", "https://www.youtube.com/embed/gzs0-N5qRtY");
+    $("#timelapseYouTube").attr("src", "https://www.youtube.com/embed/PytYNkP4hpE");
+    $("#beerbrewYouTube").attr("src", "https://www.youtube.com/embed/ff6grgUZ6C4");
   };
 
   //smooth scroll to anchor
@@ -43,13 +49,10 @@ $(function(){
   var windowHeight = $(window).height();
   $introDiv.css({height: windowHeight});
   $jobTitle.css({paddingTop: (windowHeight/2), marginTop: 0});
-  $jobTitle.html("");
-  $jobTitle.append(CONSTANTS.JOB_TITLE_SPAN);
 
-  playIntroSequence();
-  
-  //until ready for production, no timeout
-  //$("body").css({overflow: "hidden"});
-  // setTimeout(playIntroSequence, 2000);
-  
+  setTimeout(function() {
+      $jobTitle.html(CONSTANTS.JOB_TITLE_SPAN);
+      playIntroSequence();
+  }, 100);
+
 });
