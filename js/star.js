8f4aac78de6c80c3f50e47f47bac1a00a5135e44
Brent Vale <brentvale@gmail.com>
2016-04-12 11:06:32 -0700

    functioning stars coming from single point fade in

--- js/star.js
+++ js/star.js
@@ -6,15 +6,31 @@ function Star(options){
   this.x = options.x;
   this.y = options.y;
   this.radius = options.radius;
-  this.deltaX = this.createDeltaX();
-  this.deltaY = this.createDeltaY();
+  
+  this.deltaX = (this.velocity.x / 50);
+  this.deltaY = (this.velocity.y / 50);
+  this.randomAdjust();
 };
 
 Star.prototype = {
-  createDeltaX: function(){
-    debugger
-  },
-  createDeltaY: function(){
+  //adjust stars on initialize to be spread across the screen
+  randomAdjust: function(){
+    //if random number is greater than half the window height, stars are created with too many
+    //off the screen in the y-direction
+    var randomizer = Math.floor(Math.random() * WINDOW_WIDTH/2);
+    //if number is 100 units away (increments at 0.01)
+    var opacity = (randomizer > 100) ? 1.0 : randomizer * 0.01;
     
+    this.opacity = opacity;
+
+    this.x = this.x + (this.deltaX * randomizer);
+    this.y = this.y + (this.deltaY * randomizer);
+    
+    if(this.y < 0){
+      this.y = this.y * -1 * (1/Math.floor(Math.random() * 5));
+    }
+    if(this.y > WINDOW_HEIGHT/2){
+      this.y = this.y * (1/Math.floor(Math.random() * 10));
+    }
   }
 };
--- js/stars.js
+++ js/stars.js
@@ -1,16 +1,19 @@
 var TOTAL_STARS = 100;
-var COLORS = ["blue", "white"]
+var COLORS = ["blue", "white"];
+var WINDOW_HEIGHT = 0;
+var WINDOW_WIDTH = 0;
 
 
 function Stars(options){
   this.ctx = options.context;
   this.height = options.height;
   this.width = options.width;
+  this.setWindowHeight();
   this.stars = [];
-  this.clearScreen();
   this.possibleVelocities = this.generateVelocities();
   this.createStars();
   this.vanishingPoint = this.createVanishingPoint();
+  this.createAndLoadImage();
   this.tick();
 };
 
@@ -20,24 +23,46 @@ function Velocity(options){
 };
 
 Stars.prototype = {
+  setWindowHeight: function(){
+    WINDOW_HEIGHT = this.height;
+    WINDOW_WIDTH = this.width;
+  },
+  createAndLoadImage: function(){
+    var img = new Image();
+    img.src = "images/main_background.jpg";
+    STARS_BACKGROUND = img;
+  },
   clearScreen: function(){
-    this.ctx.beginPath();
-    this.ctx.rect(0, 0, this.width, this.height);
-    this.ctx.fillStyle = "black";
-    this.ctx.fill();
+    this.ctx.drawImage(STARS_BACKGROUND, 0, 0, this.width, this.height);
+    
+    // this.ctx.beginPath();
+//     this.ctx.rect(0, 0, this.width, this.height);
+//     this.ctx.fillStyle = "black";
+//     this.ctx.fill();
   },
   tick: function(){
     var that = this;
     
     setInterval(function(){
-      that.drawStars();
+      that.clearScreen();
       that.moveStars();
-    }, 1000);
+      that.drawStars();
+    }, 100);
   },
   moveStars: function(){
     for(var i = 0; i < this.stars.length; i ++){
-      debugger
-      this.stars[i]
+      this.stars[i].x = this.stars[i].x + this.stars[i].deltaX;
+      this.stars[i].y = this.stars[i].y + this.stars[i].deltaY;
+      this.stars[i].opacity = (this.stars[i].opacity < 1.0) ? this.stars[i].opacity + 0.01 : this.stars[i].opacity;
+      //reset stars to center position once they go off the screen
+      if(this.stars[i].x < 0 || this.stars[i].x > this.width || this.stars[i].y < 0 ){
+        this.stars[i].x = this.stars[i].velocity.x + this.width/2;
+        
+        this.stars[i].y = (this.stars[i].velocity.y/10) + this.height/2;
+        this.stars[i].opacity = 0;
+        
+        
+      }
     }
   },
   drawStars: function(){
@@ -46,7 +71,7 @@ Stars.prototype = {
       this.ctx.arc((this.stars[i].x), (this.stars[i].y),  this.stars[i].radius, 0, Math.PI*2);
       // this.ctx.arc(520, 204,  this.stars[i].radius, 0, Math.PI*2);
       this.ctx.closePath();
-      this.ctx.fillStyle = "white";
+      this.ctx.fillStyle = "rgba(255, 255, 255," + this.stars[i].opacity +")";
       this.ctx.fill();
     }
   },
@@ -55,12 +80,13 @@ Stars.prototype = {
       //size between one and two
       var randomSize = Math.floor(Math.random()*2 + 1);
       var randomColor = COLORS[Math.floor(Math.random() * COLORS.length)];
-      //velocities can be outward from
-      var randomVelocity = this.possibleVelocities[i];
-      var tempX = this.possibleVelocities[i].x + 100;
-      var tempY = this.possibleVelocities[i].y + 100;
-      var radius = 2;
-      this.stars.push(new Star({size: randomSize, color: randomColor, velocity:randomVelocity, x: tempX, y:tempY, radius: radius}));
+      //velocities can be outward from origin point
+      var selectedVelocity = this.possibleVelocities[i];
+      var tempX = this.possibleVelocities[i].x + this.width/2;
+      var tempY = (this.possibleVelocities[i].y)/10 + this.height/2;
+      
+      var radius = 1;
+      this.stars.push(new Star({size: randomSize, color: randomColor, velocity:selectedVelocity, x: tempX, y:tempY, radius: radius}));
     }
   },
   createVanishingPoint: function(){
@@ -72,8 +98,15 @@ Stars.prototype = {
   generateVelocities: function(){
     var velocities = [];
     for(var i = 0; i < 100; i ++){
-      velocities.push(new Velocity({x: -1*i,y:i-100}));
-      velocities.push(new Velocity({x: i,y:i-100}));
+      //adjust left side of stars off horizon
+      if(i < 70){
+        velocities.push(new Velocity({x: -1*i,y:i-100}));
+      }
+      //adjust right side of stars off horizon
+      if(i < 88){
+        velocities.push(new Velocity({x: i,y:i-100}));
+      }
+      
     }
     return velocities;
   }
