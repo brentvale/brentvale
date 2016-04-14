cec653c948b5e50c9d10b2e2bf2105f4e866f98c
Brent Vale <brentvale@gmail.com>
2016-04-14 15:20:06 -0700

    updates cover photo and adds lightning

--- js/redo_script.js
+++ js/redo_script.js
@@ -12,7 +12,7 @@ $(function(){
    $(".intro__hoverable").hover(function(event){
      $background.attr("src", "images/brent_colored_pool_lg.jpg");
    }, function(event){
-     $background.attr("src", "images/brent_bandw_pool_lg.jpg");
+     $background.attr("src", "images/background_pool_brent_sky_bella.jpg");
    });
    
   //MOBILE
@@ -66,4 +66,32 @@ $(function(){
   
   var c = canvas.getContext('2d');
   var starClusters = new Stars({context: c, height: windowHeight, width: windowWidth});
+  
+  //lightning bolt is 200x1600px, 8 fit on sprite
+  //1  2      3      4      5      6      7
+  //0, 14.28, 28.57, 42.85, 57.14, 71.43, 85.71
+  
+  $lightning = $('.intro__lightning');
+  // fraction to place lighting at correct point in image = 45/72
+  var lightTargetHeight = parseInt($('#mainBackground').css("height"))/72*45;
+  $lightning.css({width: windowWidth, height: windowWidth/8, top: (lightTargetHeight + "px")});
+  
+  
+  var lightningStrike = function(){
+    setTimeout(function(){$lightning.css({backgroundPosition: " 0 14.28%"})}, 50);
+    setTimeout(function(){$lightning.css({backgroundPosition: " 0 28.57%"})}, 100);
+    setTimeout(function(){$lightning.css({backgroundPosition: " 0 42.85%"})}, 150);
+    
+    setTimeout(function(){$lightning.css({backgroundPosition: " 0 28.57%"})}, 200);
+    setTimeout(function(){$lightning.css({backgroundPosition: " 0 42.85%"})}, 250);
+    
+    setTimeout(function(){$lightning.css({backgroundPosition: " 0 28.57%"})}, 300);
+    setTimeout(function(){$lightning.css({backgroundPosition: " 0 42.85%"})}, 350);
+    //reset lightning to blank position on sprite
+    setTimeout(function(){$lightning.css({backgroundPosition: " 0 42.85%"})}, 600);
+  };
+  setInterval(lightningStrike, 5000);
+  
+  
+  //NEED TO HANDLE WINDOW RESIZING WITH LIGHTNING
 });
